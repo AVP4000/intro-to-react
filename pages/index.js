3c861338747bb610119b6632b78765ee5d97daa1
Andre Phosarath <andrepho22@gmail.com>
2024-07-06 00:03:13 -0500

    working on counter

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
-// import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
-// import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-// import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
+import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
+import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import Counter from '../components/Counter';
 
 function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const titles = [{ title: 'AP Counter' }, { title: 'Darrell Counter' }, { title: 'Ben Counter' }, { title: null }];
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  // const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-    </div>
+    <>
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '20vh',
+          padding: '30px',
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+      >
+        <h1>Hello {user.displayName}! </h1>
+        <Button onClick={signOut}>Sign Out</Button>
+      </div>
+      {titles.map((t) => <Counter key={t.title} title={t.title} />)}
+    </>
   );
 }
 
